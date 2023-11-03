a1abaa6c4bf58674f4c6bafdd93007ca3d6ed939
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-11-03 23:31:45 +0100

    Ok

--- src/main/java/com/epam/rd/autotask/stream/intermediate/MovieQueries.java
+++ src/main/java/com/epam/rd/autotask/stream/intermediate/MovieQueries.java
@@ -4,66 +4,108 @@ import com.epam.rd.autotask.stream.intermediate.model.Genre;
 import com.epam.rd.autotask.stream.intermediate.model.Movie;
 import com.epam.rd.autotask.stream.intermediate.model.Person;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MovieQueries {
+    List<Movie> movies;
 
     public MovieQueries(List<Movie> movies) {
+        if (movies == null) throw new IllegalArgumentException();
+        this.movies = movies;
     }
 
 
     public boolean checkGenreOfAllMovies(Genre genre) {
-        return false;
+        return movies.stream()
+                .allMatch(movie -> movie.getGenres().contains(genre));
+
     }
 
     public boolean checkGenreOfAnyMovies(Genre genre) {
-        return false;
+        return movies.stream()
+                .anyMatch(movie -> movie.getGenres().contains(genre));
     }
 
     public boolean checkMovieIsDirectedBy(Person person) {
-        return false;
+        return movies.stream()
+                .flatMap(movie -> movie.getDirectors().stream())
+                .anyMatch(d -> d.equals(person));
 
     }
 
     public int calculateTotalLength() {
-        return 0;
+        return movies.stream()
+                .mapToInt(Movie::getLength)
+                .reduce(0, Integer::sum);
     }
 
     public long moviesLongerThan(int minutes) {
-        return 0;
+        return movies.stream()
+                .filter(movie -> movie.getLength() > minutes)
+                .count();
     }
 
     public List<Person> listOfWriters() {
-        return null;
+        return movies.stream()
+                .flatMap(movie -> movie.getWriters().stream())
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public String[] movieTitlesWrittenBy(Person person) {
-        return null;
+        List<Person> people = movies.stream()
+                .flatMap(movie -> movie.getWriters().stream())
+                .filter(p -> p.equals(person))
+                .collect(Collectors.toList());
+        String[] persons = new String[people.size()];
+        return people.toArray(persons);
     }
 
     public List<Integer> listOfLength() {
-        return null;
+        return movies.stream()
+                .map(Movie::getLength)
+                .collect(Collectors.toList());
     }
 
     public Movie longestMovie() {
-        return null;
+        if (movies.isEmpty()) throw new IllegalArgumentException();
+
+        var sorted = movies.stream()
+                .sorted(Comparator.comparing(Movie::getLength).reversed())
+                .collect(Collectors.toList());
+        return sorted.get(0);
+
     }
 
     public Movie oldestMovie() {
-        return null;
+        if (movies.isEmpty()) throw new IllegalArgumentException();
+        var sorted = movies.stream()
+                .sorted(Comparator.comparing(Movie::getReleaseYear))
+                .collect(Collectors.toList());
+        return sorted.get(0);
     }
 
     public List<Movie> sortedListOfMoviesBasedOnReleaseYear() {
-        return null;
+        return movies.stream()
+                .sorted(Comparator.comparing(Movie::getReleaseYear))
+                .collect(Collectors.toList());
     }
 
     public List<Movie> sortedListOfMoviesBasedOnTheDateOfBirthOfOldestDirectorsOfMovies() {
-        return null;
+        List<Movie> list = new ArrayList<>();
+        for (Movie movie : movies) {
+            movie.getWriters().sort(Comparator.comparing(Person::getDateOfBirth));
+            list.add(movie);
+        }
+        return list;
     }
 
     public List<Movie> moviesReleasedEarlierThan(int releaseYear) {
-        return null;
+        return movies.stream()
+                .filter(movie -> movie.getReleaseYear() <= releaseYear)
+                .collect(Collectors.toList());
     }
 }
